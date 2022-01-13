1596ae41ac037b8a150bb14328891ef726264554
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2022-01-13 13:58:21 +0200

    start-hm-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
-
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+const baseUrl = 'https://restcountries.com/v3.1/name/{name}';
 const DEBOUNCE_DELAY = 300;
+
+function getDataServer(url) {
+    return fetch(url).then(response => response.json())
+}
+getDataServer(baseUrl).then(data => {
+    const markup;
+})
